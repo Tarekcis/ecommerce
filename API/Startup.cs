b394a7b2663525265192e6e3745a1156566bc524
Tarek.QBS <Tarek.QBS@DESKTOP-6ISIA92>
2021-10-05 22:31:27 +0200

    End of section 6

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,14 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,7 +56,7 @@ namespace API
 
             app.UseRouting();
             app.UseStaticFiles();
-
+app.UseCors("CorsPolicy");
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
