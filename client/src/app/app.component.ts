0e271324cf223f309e80c632b240e395198c4f61
Tarek.QBS <Tarek.QBS@DESKTOP-6ISIA92>
2021-10-08 16:06:06 +0200

    End Of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,23 @@
-import {ChangeDetectionStrategy, Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
+  constructor(private http:HttpClient){
+  }
+  products:IProduct[];
+  ngOnInit(): void {
+    this .http.get('https://localhost:5001/api/products?pageSize=20').subscribe((respose:IPagination) =>{
+      this.products= respose.data;
+      })
+  }
+
   title = 'SkiNet';
-  radioModel = 'Middle';
-  radioModelDisabled = 'Middle';
-  modelGroupDisabled=false
+ 
 }
